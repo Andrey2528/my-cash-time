3626c65815e4ec341beb15c1c68762f5848e489d
Oleg Ivanov <oleg.ivanov@waites.net>
2022-10-25 01:20:36 +0300

    set up webpack

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import ReactDOM from "react-dom";
+import App from "./app.jsx";
+
+const app = ReactDOM.render(<App />, document.querySelector("#app"));
---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlMinimizerPlugin = require("html-minimizer-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: process.env.NODE_ENV == "production" ? "production" : "development",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[fullhash].bundle.js",
+    clean: true,
+  },
+  devServer: {
+    port: process.env.PORT || 3000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new MiniCssExtractPlugin({ filename: "[name].[fullhash].bundle.css" }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+          },
+        },
+      },
+      {
+        test: /\.(tsx|ts)$/,
+        loader: "ts-loader",
+        exclude: "/node_modules/",
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|webp|ico)$/i,
+        type:
+          process.env.NODE_ENV === "production" ? "asset" : "asset/resource",
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new HtmlMinimizerPlugin(), new CssMinimizerPlugin()],
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+};
