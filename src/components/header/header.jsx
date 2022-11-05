bc73ad0fd124809b23f98f3c3c09495d5f6004f6
Andrey2528 <andreys20.2016@ukr.net>
2022-11-05 12:22:43 +0200

    Update header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,12 +1,28 @@
 import React from "react";
-import Header_layouts from "@/layouts/header/header_layout.jsx";
 import "./header.scss";
 
 const Header = () => {
     return (
         <header className="header">
             <div className="container">
-                <Header_layouts></Header_layouts>
+                <nav className="navbar">
+                    <div className="navbar__row">
+                        <p className="main__title">MY CASH&amp;TIME</p>
+                        <ul className="navbar__list">
+                            <li className="navbar__item">
+                                <p
+                                    className="main__desc"
+                                    style={{ fontWeight: "700" }}
+                                >
+                                    Cash
+                                </p>
+                            </li>
+                            <li className="navbar__item">
+                                <p className="main__desc">Time</p>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
             </div>
         </header>
     );
